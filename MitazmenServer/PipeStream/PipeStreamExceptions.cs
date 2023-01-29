c68ffe2c81b41e4d9d7b467ef1bb784a94f78835
Mhemmad Abdalla <mhemmad.abdalla@zerto.com>
2023-01-29 16:24:54 +0200

    Adding Name Pipe stream

---
+++ MitazmenServer/PipeStream/PipeStreamExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MitazmenServer.PipeStream
+{
+    public class PipeStreamInvalidPipeObjectException : Exception
+    {
+        public PipeStreamInvalidPipeObjectException(string e): base(e)
+        {
+
+        }
+    }
+}
---
+++ MitazmenServer/PipeStream/StreamString.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace PipeStream
+{
+    public class StreamString
+    {
+        private Stream ioStream;
+        private UnicodeEncoding streamEncoding;
+
+        public StreamString(Stream ioStream)
+        {
+            this.ioStream = ioStream;
+            streamEncoding = new UnicodeEncoding();
+        }
+
+        public string ReadString()
+        {
+            int len = 0;
+
+            len = ioStream.ReadByte() * 256;
+            len += ioStream.ReadByte();
+            byte[] inBuffer = new byte[len];
+            ioStream.Read(inBuffer, 0, len);
+
+            return streamEncoding.GetString(inBuffer);
+        }
+
+        public int WriteString(string outString)
+        {
+            byte[] outBuffer = streamEncoding.GetBytes(outString);
+            int len = outBuffer.Length;
+            if (len > ushort.MaxValue)
+            {
+                len = ushort.MaxValue;
+            }
+            ioStream.WriteByte((byte)(len / 256));
+            ioStream.WriteByte((byte)(len & 255));
+            ioStream.Write(outBuffer, 0, len);
+            ioStream.Flush();
+
+            return outBuffer.Length + 2;
+        }
+    }
+    public class ReadFileToStream
+    {
+        private string fn;
+        private StreamString ss;
+
+        public ReadFileToStream(StreamString str, string filename)
+        {
+            fn = filename;
+            ss = str;
+        }
+
+        public void Start()
+        {
+            string contents = File.ReadAllText(fn);
+            ss.WriteString(contents);
+        }
+    }
+}
